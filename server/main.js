b40b4229b579fe75561fbee7567fb14623fe5d90
StamenDoC <nidza_94@hotmail.com>
2016-11-30 20:53:47 +0100

    Add files via upload

---
+++ server/main.js
@@ -0,0 +1,12 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+
+Meteor.startup(() => {
+  // code to run on server at startup
+
+/*OVDE PISEMO KOD ZA SLANJE AKTIVACIONOG EMAIL-A*/
+
+ /*Accounts.config({
+        sendVerificationEmail: true
+    });*/
+});
