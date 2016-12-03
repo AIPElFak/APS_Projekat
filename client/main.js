859fd7a8677c437c0991a785aed5675d28ba7cd0
StamenDoC <nidza_94@hotmail.com>
2016-12-03 19:14:50 +0100

    Add files via upload

--- client/main.js
+++ client/main.js
@@ -1,3 +1,5 @@
+/*TREAB DA SE nAPRAVI AKO JE VEC LOGOVAN DA NE MOZE DA ODE NA POCETNU STRANU NEGO DA GA UBACUJE ODMAH U LOBY I DA MU SE STAVI NEKO OGRANICENJE NA SESIJU AKO NISTA NE PRETISKA*/
+
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
@@ -24,16 +26,121 @@ Router.route('/loby',{
 
 if(Meteor.isClient)
 {
+	/*Meteor.logout();
+	console.log(Meteor.userId());*/
+	console.log(Meteor.userId());
+
+
+	//Froma za registraciju
 	Template.register.events({
+		//zabranjujem space karaktere
+		'keypress .registerForm': function(event){
+			var code = event.charCode || event.keyCode;
+		  	if(code == 32)
+		    	return false;
+		},
 	    'submit .registerForm': function(event){
 	        event.preventDefault();
+	        var username = event.target.username.value;
 	        var email = event.target.email.value;
 	        var password = event.target.password.value;
+	        var password_confirm = event.target.password_confirm.value;
 
-	        Accounts.createUser({
-	            email: email,
-	            password: password
-	        });      
+	        if(password == password_confirm)
+	        {
+		        Accounts.createUser({
+		        	username: username,
+		            email: email,
+		            password: password
+		        }, function(error){
+					    // code goes here    
+					    if(error){
+					    	$("#errorRegister").html("<p style='color:red;'>"+error.reason+"</p>");
+					        console.log(error.reason); // Output error if registration fails
+					    }
+
+					    else
+					    {
+					    	Router.go('home');
+					    }
+				});
+	        	//Router.go('loby');   
+	       	}
+
+	       	else
+	       	{
+	       		$("#errorRegister").html("<p style='color:red;'>Password no match</p>");
+	       	}
+	    }
+	});
+
+	Template.home.events({
+		//zabranjujem space karaktere
+		'keypress .loginForm': function(event){
+			var code = event.charCode || event.keyCode;
+		  	if(code == 32)
+		    	return false;
+		},
+	    'submit .loginForm': function(event){
+	        event.preventDefault();
+	        var username = event.target.username.value;
+	        var password = event.target.password.value;
+
+	        Meteor.loginWithPassword(username, password, function(error){
+			    if(error)
+			    {
+			        $("#errorLogin").html("<p style='color:red;'>"+error.reason+"</p>");
+			    } 
+			    else 
+			    {
+			        Router.go('loby');
+			    }
+			});
+	    },
+	    'click .facebookLogin': function(event){
+
+	    	Meteor.loginWithFacebook({
+			  requestPermissions: ['user_friends', 'public_profile', 'email']
+			}, (err) => {
+			  if (err) {
+			    // handle error
+			    $("#errorLogin").html("<p style='color:red;'>"+err.reason+"</p>");
+			  } else {
+			    // successful login!
+			    Router.go('loby');
+			  }
+			});
+	    },
+
+	    'click .googleLogin': function(event){
+
+	    	//Meteor.loginWithGoogle();
+	    	Meteor.loginWithGoogle({
+			  requestPermissions: ['email']
+			}, (err) => {
+			  if (err) {
+			    // handle error
+			    $("#errorLogin").html("<p style='color:red;'>"+err.reason+"</p>");
+			  } else {
+			    // successful login!
+			    Router.go('loby');
+			  }
+			});
+	    },
+
+	    'click .twitterLogin': function(event){
+
+	    	Meteor.loginWithTwitter({
+			  requestPermissions: ['user_friends', 'public_profile', 'email']
+			}, (err) => {
+			  if (err) {
+			    // handle error
+			    $("#errorLogin").html("<p style='color:red;'>"+err.reason+"</p>");
+			  } else {
+			    // successful login!
+			    Router.go('loby');
+			  }
+			});
 	    }
 	});
 }
--- server/main.js
+++ server/main.js
@@ -2,8 +2,37 @@ import { Meteor } from 'meteor/meteor';
 import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
-  // code to run on server at startup
 
+	ServiceConfiguration.configurations.upsert({
+  	service: "facebook"
+	}, {
+	  $set: {
+	    appId: "1022696844524002",
+	    loginStyle: "popup",
+	    secret: "a3763c673ba0d39a6028d89cb280c009"
+	  }
+	});
+
+	ServiceConfiguration.configurations.upsert({
+  	service: "google"
+	}, {
+	  $set: {
+	    clientId: "142824363488-lo1h69v8l5jmibnag5nn6tjrhn9ha8se.apps.googleusercontent.com",
+	    loginStyle: "popup",
+	    secret: "cYc8dV3k9Xen03pTBEOIdhri"
+	  }
+	});
+
+	ServiceConfiguration.configurations.upsert({
+  	service: "twitter"
+	}, {
+	  $set: {
+	    consumerKey: "w9izu9W2hcZCg7BMV0bfmtssQ",
+	    loginStyle: "popup",
+	    secret: "jWIBkPdbgzWVaZ3M3UIktOFPoRwYnvkpMYX18Zqnd72B8k8jAZ"
+	  }
+	});
+  // code to run on server at startup
 /*OVDE PISEMO KOD ZA SLANJE AKTIVACIONOG EMAIL-A*/
 
  /*Accounts.config({
